83a4480d617eb7fef68179ee839b07cc9524a207
LUKAS ERWINDO SNAKE <54299519+Lukaserwindo69@users.noreply.github.com>
2019-11-20 20:41:55 +0700

    Add files via upload

---
+++ nilai siwa/src/nilai/siwa/NilaiSiwa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nilai.siwa;
+
+/**
+ *
+ * @author KANG_SNAKE
+ */
+public class NilaiSiwa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
